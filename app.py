cb67de438ade94314db4cb4b44330d00da121102
Renato M. Mendes <renatomaquesmendes@gmail.com>
2025-02-10 16:44:47 -0300

    alterando o app

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def index():
     return render_template('index.html', resposta=resposta,frase=frase)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8087)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
