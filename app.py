3f3717ef3b10e96733d0ca3a901db8ee3f2afb07
Renato M. Mendes <renatomaquesmendes@gmail.com>
2025-02-24 17:27:13 -0300

    Adicionando novas páginas

--- app.py
+++ app.py
@@ -74,6 +74,14 @@ def index():
 
     return render_template('index.html', resposta=resposta,frase=frase)
 
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
